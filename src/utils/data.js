ab108aa569b2998023a867987e29fee911f0302c
aqeelyousaf <maheraqeel627@gmail.com>
2024-08-20 23:07:21 +0500

    my portfolio

---
+++ src/utils/data.js
@@ -0,0 +1,166 @@
+import { HiOutlineDesktopComputer } from "react-icons/hi";
+import { CiMobile1 } from "react-icons/ci";
+import { MdWorkspacesOutline } from "react-icons/md";
+export const projectExperience = [
+  {
+    name: "React.js Projects",
+    projects: 5,
+    icon: CiMobile1,
+    bg: "#FFA51F",
+  },
+  {
+    name: "WordPress Projects",
+    projects: 2,
+    icon: HiOutlineDesktopComputer,
+    bg: "#286F6C",
+  },
+  {
+    name: "Javascript Projects",
+    projects: 8,
+    icon: MdWorkspacesOutline,
+    bg: "#F26440",
+  },
+  {
+    name: "Simple HTML,CSS,BOOTSTRAP",
+    projects: 10,
+    icon: HiOutlineDesktopComputer,
+    bg: "#62489A",
+  },
+];
+
+export const WhatDoIHelp = [
+  "As a frontend web developer with expertise inJavascript and React.js, , I can help you build engaging and responsive websites that are tailored to your specific needs. Whether you are looking to create a dynamic and interactive web application using React  , I can help you bring your ideas to life. With my experience in HTML, CSS, JavaScript, BootStrap, React.js I can design and develop intuitive user interfaces that are optimized for user experience and performance.",
+];
+
+export const workExp = [
+  {
+    place: "University Semester Projects",
+    tenure: "2020-2024",
+    role: "University Projects Experience",
+    detail:
+      "When working on university semester projects as a front-end developer, your focus is on designing and implementing the user interface (UI) and ensuring a smooth user experience (UX).",
+  },
+  {
+    place: "Personal Clients",
+    tenure: "2023-2024",
+    role: "Final Year Projects For Students",
+    detail:
+      "As a React JS frontend developer, i have done projects like (Job Seeking Portal && StichHub Stiching Website) for clients"},
+  {
+    place: "Small Projects in Javascript",
+    tenure: "2022-2024",
+    role: "Javascript Projects",
+    detail:
+      "Build a weather app that fetches real-time weather data from an API like OpenWeatherMap.Search for weather by city.Display current temperature, weather conditions, and an icon.Create a simple to-do list where users can add, edit, and delete tasks.Develop a quiz app that asks multiple-choice questions and tracks the user’s score.",
+  },
+  {
+    place: "WordPress Knowledge",
+    tenure: "2021-2022",
+    role: "WordPress Developer",
+    detail:
+      "As a WordPress developer, I have some knowledge about wordpress .In wordpress  developer role involve creating intuitive and user-friendly interfaces that are optimized for speed and performance.",
+  },
+];
+
+export const comments = [
+  {
+    name: "Zarmo ric",
+    post: "United State",
+    comment:
+      "Aqeel Yousaf designed everything I needed, down to the period. Great job thank you.",
+    img: "./person.png",
+  },
+  {
+    name: "Anamika Sandula",
+    post: "Indian",
+    comment:
+      "Incredibly Amazing experience. I am NOT going to any other developer for my futur projects. I will advertise that people should pick Aqeel Yousaf  as their goto guy for dedicated, professional, highly capable and relentless partner.",
+    img: "./people1.png",
+  },
+  {
+    name: "Jonerb",
+    post: "United Kingdom",
+    comment:
+      "Another home run!!! Can’t get enough of the talent, willingness and determination of Aqeel Yousaf .",
+    img: "./people2.png",
+  },
+  {
+    name: "Allanzheng",
+    post: "United Kingdom",
+    comment:
+      "Aqeel Yousaf  is the best! He is a true professional who meets his deadlines and is super knowledgeable about front-end and back-end technologies. Definitely recommend!.",
+    img: "./person.png",
+  },
+  {
+    name: "Sajid khan",
+    post: "Pakistan",
+    comment:
+      "This was my second job from the developer, understood what I wanted quickly and finished the work within few hours. Great experience!",
+    img: "./people3.jpg",
+  },
+  {
+    name: "Grace writes",
+    post: "United State",
+    comment:
+      "Best developer I’ve met online, very helpful and instant communication, Will recommend to everyone.",
+    img: "./people1.png",
+  },
+  {
+    name: "Jones",
+    post: "United State",
+    comment: "better than advertised.",
+    img: "./people2.png",
+  },
+  {
+    name: "Daniele",
+    post: "Bangladesh",
+    comment:
+      "Very easy-going developer. Love everything he took the time to exactly what I was requested. Many thanks again Aqeel.",
+    img: "./person.png",
+  },
+  {
+    name: "Omararrale",
+    post: "United Kingdom",
+    comment:
+      "Aqeel Yousaf  understood straightaway what I was asking for and got the job done with time. Asad communicated each step he took.",
+    img: "./people3.jpg",
+  },
+];
+
+export const sliderSettings = {
+  dots: true,
+  infinite: false,
+  speed: 1000,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  initialSlide: 0,
+  touchMove: true,
+  useCSS: true,
+
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true,
+      },
+    },
+    {
+      breakpoint: 1000,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        initialSlide: 2,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+};
